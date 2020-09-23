7d07f94543f1c3649521d017238e1ecdb2b5394d
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-09-23 23:14:17 +0200

    next

---
+++ src/test/java/com/wojciech/gaudnik/spock_test/service/EmployeeServiceBDDTest.java
@@ -0,0 +1,78 @@
+package com.wojciech.gaudnik.spock_test.service;
+
+import com.wojciech.gaudnik.spock_test.model.Employee;
+import com.wojciech.gaudnik.spock_test.model.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceBDDTest {
+
+	@Mock
+	EmployeeRepository employeeRepository;
+
+	@InjectMocks
+	EmployeeService employeeService;
+
+	@Test
+	void getEmployeeByNameTest() {
+		//given
+		var em1 = Employee.builder().id(1L).name("test1").build();
+		var em2 = Employee.builder().id(1L).name("test2").build();
+		given(employeeRepository.findByName("test1")).willReturn(em1);
+//		when(employeeRepository.findByName("test1")).thenReturn(em1);
+
+		//when
+		var result = employeeService.getEmployeeByName("test1");
+//		when(employeeService.getEmployeeByName("test1")).thenReturn(em2); //todo <--- doesn't work
+
+		//then
+		then(employeeRepository).should().findByName(anyString());
+		then(employeeRepository).shouldHaveNoMoreInteractions();
+		assertThat(result).isNotNull();
+	}
+
+	@Test
+	void testDoThrow() {
+		doThrow(new RuntimeException("boom")).when(employeeRepository).delete(any());
+
+		var em1 = new Employee("test");
+		assertThrows(RuntimeException.class, () -> employeeRepository.delete(em1));
+
+		verify(employeeRepository).delete(any());
+
+	}
+
+	@Test
+	void testFindByNameThrows() {
+		given(employeeRepository.findByName("test1")).willThrow(new RuntimeException("boom"));
+
+		assertThrows(RuntimeException.class, () -> employeeService.getEmployeeByName("test1"));
+
+		then(employeeRepository).should().findByName("test1");
+	}
+
+	@Test
+	void testDeleteThrowsBDD() {
+		willThrow(new RuntimeException("boom")).given(employeeRepository).delete(any());
+
+		var em1 = new Employee("test1");
+		assertThrows(RuntimeException.class, () -> employeeRepository.delete(em1));
+
+		then(employeeRepository).should().delete(any());
+	}
+}
