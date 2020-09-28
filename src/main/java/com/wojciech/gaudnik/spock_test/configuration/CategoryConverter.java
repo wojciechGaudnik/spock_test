895281a16cff55f27870e537e4060811ee43a551
Wojciech Gaudnik <wojciech.gaudnik@enigma.com.pl>
2020-09-28 07:49:46 +0200

    next

---
+++ src/main/java/com/wojciech/gaudnik/spock_test/configuration/CategoryConverter.java
@@ -0,0 +1,27 @@
+package com.wojciech.gaudnik.spock_test.configuration;
+
+//package pl.com.enigma.centaurdat.admin.configuration;
+//
+//@Converter(autoApply = true)
+//public class CategoryConverter implements AttributeConverter<Category, String> {
+//
+//	@Override
+//	public String convertToDatabaseColumn(Category category) {
+//		if (category == null) {
+//			return null;
+//		}
+//		return category.getCode();
+//	}
+//
+//	@Override
+//	public Category convertToEntityAttribute(String code) {
+//		if (code == null) {
+//			return null;
+//		}
+//
+//		return Stream.of(Category.values())
+//				.filter(c -> c.getCode().equals(code))
+//				.findFirst()
+//				.orElseThrow(IllegalArgumentException::new);
+//	}
+//}
